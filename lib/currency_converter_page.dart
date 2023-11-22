c0ec1feaa5c3c78f17611df826a3c43c1c1720e5
Akshar Kalathiya <147808278+Akshar062@users.noreply.github.com>
2023-11-22 18:26:10 +0530

    second commit

--- lib/currency_converter_page.dart
+++ lib/currency_converter_page.dart
@@ -15,69 +15,72 @@ class _CurrencyConverterMaterialPageState
   double result = 0;
   final TextEditingController textEditingController = TextEditingController();
 
-  final TextEditingController resultEditingController =
-  TextEditingController();
-
-  // Map to store conversion rates for each currency
-  final Map<String, double> conversionRates = {
-    'USD': 1.0, // Default rate for USD
-    'Rupees': 0.0, // Set to 0 initially
-    'Euro': 0.0,
-    'Pound': 0.0,
-    // Add more currencies and their rates as needed
-  };
-
-  Future<void> fetchExchangeRate(String baseCurrency, String targetCurrency) async {
+  Future<void> fetchExchangeRate() async {
     try {
-      const apiKey = '11831825d93d96968315e4fc6957b00a'; // Replace with your API key
-      final url = Uri.parse(
-          'https://api.exchangerate-api.com/v4/latest/$baseCurrency?access_key=$apiKey');
+      final apiKey = '11831825d93d96968315e4fc6957b00a'; // Replace with your API key
+      final url = Uri.parse('https://api.exchangerate-api.com/v4/latest/USD?access_key=$apiKey');
 
-      final response = await http.get(url).timeout(const Duration(seconds: 5));
+      final response = await http.get(url);
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-
-        // Update conversion rates from the API
-        conversionRates[targetCurrency] = data['rates'][targetCurrency];
-
-        // Perform the conversion using the selected currency rate
-        _updateResult(double.parse(textEditingController.text) *
-            conversionRates[targetCurrency]!);
+        final rate = data['rates']['INR'];
+        setState(() {
+          result = double.parse(textEditingController.text) * rate;
+        });
       } else {
-        _handleError('Failed to fetch exchange rate');
+        setState(() {
+          result = 010;
+        });
+        throw Exception('Failed to fetch exchange rate');
       }
     } catch (e) {
-      _handleError('Error fetching exchange rate: $e');
+      // Handle exceptions
+      if (kDebugMode) {
+        print('Error fetching exchange rate: $e');
+      }
     }
   }
 
-  void _updateResult(double value) {
-    setState(() {
-      result = value;
-      resultEditingController.text = result.toStringAsFixed(2);
-    });
-
-    // Print statements for debugging
-    print('Updated Result: $result');
-    print('Controller Value: ${resultEditingController.text}');
-  }
-
-
-  void _handleError(String message) {
-    setState(() {
-      result = 0; // Set to a default value or display an error message.
-      resultEditingController.text = result.toStringAsFixed(2);
-    });
-
-    if (kDebugMode) {
-      print(message);
+  void updateResult() {
+    try {
+      setState(() {
+        result = double.parse(textEditingController.text) * 81;
+      });
+    } catch (e) {
+      // Handle the case when the entered value is not a valid double
+      if (kDebugMode) {
+        print('Invalid input: ${textEditingController.text}');
+      }
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.white70,
+        width: 1,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+    );
+
+    const hintStyle = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      color: Colors.white24,
+    );
+
+    const labelStyle = TextStyle(
+      fontWeight: FontWeight.w500,
+      color: Colors.white70,
+    );
+    const TextStyle textStyle = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.white70,
+    );
+
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
@@ -93,40 +96,112 @@ class _CurrencyConverterMaterialPageState
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _buildCurrencyField(
-            label: 'USD',
-            hintText: 'Enter USD',
-            prefixIcon: Icons.attach_money,
-            controller: textEditingController,
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            height: 80,
+            child: TextField(
+              controller: textEditingController,
+              style: textStyle,
+              decoration: const InputDecoration(
+                labelText: 'USD',
+                labelStyle: labelStyle,
+                hintText: 'Enter USD',
+                hintStyle: hintStyle,
+                prefixIcon: Icon(
+                  Icons.attach_money,
+                  color: Colors.white70,
+                ),
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+            ),
           ),
-          _buildCurrencyField(
-            label: 'Rupees',
-            hintText: 'Rupees',
-            prefixIcon: Icons.currency_rupee,
-            controller: resultEditingController,
-            readOnly: true,
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            height: 80,
+            child: TextField(
+              readOnly: true,
+              style: textStyle,
+              decoration: const InputDecoration(
+                labelText: 'Rupees',
+                labelStyle: labelStyle,
+                hintText: 'Rupees',
+                hintStyle: hintStyle,
+                prefixIcon: Icon(
+                  Icons.currency_rupee,
+                  color: Colors.white70,
+                ),
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+              controller: TextEditingController(
+                text: result.toStringAsFixed(2),
+              ),
+            ),
           ),
-          _buildCurrencyField(
-            label: 'Euro',
-            hintText: 'Euro',
-            prefixIcon: Icons.euro,
-            controller: resultEditingController,
-            readOnly: true,
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            height: 80,
+            child: TextField(
+              readOnly: true,
+              style: textStyle,
+              decoration: const InputDecoration(
+                labelText: 'Euro',
+                labelStyle: labelStyle,
+                hintText: 'Euro',
+                hintStyle: hintStyle,
+                prefixIcon: Icon(
+                  Icons.euro,
+                  color: Colors.white70,
+                ),
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+              controller: TextEditingController(
+                text: (result / 0.85).toStringAsFixed(2),
+              ),
+            ),
           ),
-          _buildCurrencyField(
-            label: 'Pound',
-            hintText: 'Pound',
-            prefixIcon: Icons.paid,
-            controller: resultEditingController,
-            readOnly: true,
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            height: 80,
+            child: TextField(
+              readOnly: true,
+              style: textStyle,
+              decoration: const InputDecoration(
+                labelText: 'Pound',
+                labelStyle: labelStyle,
+                hintText: 'Pound',
+                hintStyle: hintStyle,
+                prefixIcon: Icon(
+                  Icons.paid,
+                  color: Colors.white70,
+                ),
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+              controller: TextEditingController(
+                text: (result / 0.72).toStringAsFixed(2),
+              ),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
               onPressed: () {
-                fetchExchangeRate('USD', 'Rupees');
-                fetchExchangeRate('USD', 'Euro');
-                fetchExchangeRate('USD', 'Pound');
+                fetchExchangeRate();
               },
               style: TextButton.styleFrom(
                 backgroundColor: Colors.black54,
@@ -150,61 +225,4 @@ class _CurrencyConverterMaterialPageState
       ),
     );
   }
-}
-
-
-Widget _buildCurrencyField({
-  required String label,
-  required String hintText,
-  required IconData prefixIcon,
-  required TextEditingController controller,
-  bool readOnly = false,
-}) {
-  return Container(
-    padding: const EdgeInsets.all(10.0),
-    height: 80,
-    child: TextField(
-      controller: controller,
-      style: const TextStyle(
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-        color: Colors.white70,
-      ),
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: const TextStyle(
-          fontWeight: FontWeight.w500,
-          color: Colors.white70,
-        ),
-        hintText: hintText,
-        hintStyle: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w500,
-          color: Colors.white24,
-        ),
-        prefixIcon: Icon(
-          prefixIcon,
-          color: Colors.white70,
-        ),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.white70,
-            width: 1,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-        ),
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.white70,
-            width: 1,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-        ),
-      ),
-      keyboardType: const TextInputType.numberWithOptions(
-        decimal: true,
-      ),
-      readOnly: readOnly,
-    ),
-  );
 }
